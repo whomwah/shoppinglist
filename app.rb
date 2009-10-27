0c03e5f215ce34fecec37d0a0954d26a2dcaed60
Duncan Robertson <duncan@whomwah.com>
2009-10-27 17:04:26 +0000

    updates

--- app.rb
+++ app.rb
@@ -94,15 +94,13 @@ end
 get('/shoppinglist') { 
   #response["Cache-Control"] = "max-age=86400, public" 
   begin
-    r = ShoppingList.new(params["r"])
+    @data = ShoppingList.new(params["r"])
     if !iphone_request?
       content_type 'text/html', :charset => 'utf-8'
-      @title = r.title
-      @items = r.for_iphone
       erb :iphone
     else
       content_type 'text/plain', :charset => 'utf-8'
-      r.to_txt
+      @data.to_txt
     end 
   rescue OpenURI::HTTPError => e
     halt "Oo-la-la! there was something wrong with the URL you provided" 
