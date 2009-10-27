0848ba18fda13a7042b9b5971e2d0ac944a4eec2
Duncan Robertson <duncan@whomwah.com>
2009-10-27 22:27:47 +0000

    update views

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class ShoppingList
 
   def initialize(r)
     @doc = nil
-    @output = [] 
+    @data = [] 
     @title = nil 
 
     build_shopping_list(r)
@@ -35,10 +35,10 @@ class ShoppingList
 
     # The pages are so badly marked up this is really
     # hoping for the best. Hopefully it exposes the
-    # need for these pages to be fixed 
+    # need for these pages to be fixed. Here I'm just storing
+    # a loose data structure 
 
-    #@title = @doc.css('h1').first.content
-    @title = "hello" 
+    @title = @doc.css('title').first.content.split(':').last
 
     ignore_next_p = false 
     @doc.css('div[class*="content-main"] > div.promo > *').each do |el|
@@ -61,20 +61,7 @@ class ShoppingList
       end
     end
 
-    # now attempt to clean up whats left
-    items.map! do |el|
-      if el.node_name == 'h2'
-        "#{el.content.strip}<br/><br/>"
-      else
-        el
-      end
-    end
-
-    # now convert into a string and replace the br's with \n's
-    results = items.to_s.gsub(/<strong>|<\/strong>/, '<br/>')
-    results = results.split(/<br\/*>/).map! {|i| i.strip.gsub(/<\/?[^>]*>/,'')}
-
-    @output = results 
+    @data = items 
   end
 
   def fetch_recipe(url) 
@@ -82,11 +69,57 @@ class ShoppingList
   end
 
   def to_txt
-    @output.join("\n").strip
+    results = @data 
+
+    results.map! do |el|
+      if el.node_name == 'h2'
+        "#{el.content.strip}<br/><br/>"
+      else
+        el
+      end
+    end
+
+    results = results.to_s.gsub(/<strong>|<\/strong>/, '<br/>')
+    results = results.split(/<br\/*>/).map! {|i| i.strip.gsub(/<\/?[^>]*>/,'')}
+    results.join("\n").strip
   end
 
   def for_iphone
-    @output
+    d = @data 
+    results = []
+
+    d.map! do |el|
+      if el.node_name == 'h2'
+        "#{el.content.strip}<br/><br/>"
+      else
+        el
+      end
+    end
+
+    d = d.to_s.gsub(/<strong>/, '<br/>')
+    d = d.split(/<br\/*>/).map! {|i| i.strip.gsub(/<\/?[^>]*>/,'')}
+
+    has_blank = false 
+    d.each do |item|
+      if item == ''
+        has_blank = true 
+        next
+      end
+
+      if has_blank
+        has_blank = false
+        results << {
+          :is_heading => true,
+          :content => item.gsub(':','').strip
+        } 
+      else
+        results << {
+          :content => item.strip
+        } 
+      end
+    end
+
+    results
   end
 
 end
@@ -95,7 +128,7 @@ get('/shoppinglist') {
   #response["Cache-Control"] = "max-age=86400, public" 
   begin
     @data = ShoppingList.new(params["r"])
-    if !iphone_request?
+    if iphone_request?
       content_type 'text/html', :charset => 'utf-8'
       erb :iphone
     else
