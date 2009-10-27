1bef57b2c829fb748917068222d9dc37d57d8d8b
Duncan Robertson <duncan@whomwah.com>
2009-10-27 09:29:30 +0000

    got matching working for the majority

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'rubygems' 
 require 'nokogiri'
 require 'open-uri'
+require 'sinatra'
+
+SL_URI = "http://localhost/~duncan/sl1.shtml"
 
 class ShoppingList
 
@@ -20,11 +23,47 @@ class ShoppingList
   end
 
   def build_output 
-    # I'm sure this could be done better, but it works
-    d = @doc.css('div[class*="content-main"] > div.promo > p')[0].to_s
-    d = d.gsub(/<strong>|<\/strong>/, '<br>')
-    items = d.split('<br>').map! {|i| i.gsub(/<\/?[^>]*>/,'')}
-    @output << items[1...items.length] 
+    items = []
+
+    # The pages are so badly marked up this is really
+    # hoping for the best. Hopefully it exposes the
+    # need for these pages to be fixed 
+
+    ignore_next_p = false 
+    @doc.css('div[class*="content-main"] > div.promo > *').each do |el|
+      break if el.node_name == 'div'
+
+      if el.node_name == 'h2'
+        if el.content =~ /description|method/i
+          ignore_next_p = true
+          next
+        elsif el.content =~ /ingredients/i
+          next
+        end
+      end
+
+      if ignore_next_p
+        ignore_next_p = false
+        next
+      else
+        items <<  el
+      end
+    end
+
+    # now attempt to clean up whats left
+    items.map! do |el|
+      if el.node_name == 'h2'
+        "#{el.content.strip}<br/><br/>"
+      else
+        el
+      end
+    end
+
+    # now convert into a string and replace the br's with \n's
+    results = items.to_s.gsub(/<strong>|<\/strong>/, '<br/>')
+    results = results.split(/<br\/*>/).map! {|i| i.strip.gsub(/<\/?[^>]*>/,'')}
+
+    @output << results 
   end
 
   def fetch_recipe(url) 
@@ -32,13 +71,13 @@ class ShoppingList
   end
 
   def to_txt
-    @output.join("\n")
+    @output.join("\n").strip
   end
 
 end
 
 get('/shoppinglist') { 
-  response["Cache-Control"] = "max-age=86400, public" 
+  #response["Cache-Control"] = "max-age=86400, public" 
   content_type 'text/plain', :charset => 'utf-8'
   if r = ShoppingList.new(params["r"])
     r.to_txt
@@ -48,14 +87,15 @@ get('/shoppinglist') {
 }
 
 get('/') { 
-  response["Cache-Control"] = "max-age=86400, public" 
+  #response["Cache-Control"] = "max-age=86400, public" 
   content_type 'text/plain', :charset => 'utf-8'
   <<EOF
 Shopping List
 -------------
 
-Find tips for your garden any day of the year. You can subscribe to
-with the complete planner, or with optional sections listed at the bottom:
+This app attempts to take a BBC Recipe url from http://XXX and convert it
+into a list of ingedients that can be viewed on your phone, specifically
+the iPhone because that's what I've got.
 
 http://#{request.env['HTTP_HOST']}/shoppinglist?r=
 
