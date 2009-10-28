bd7a1314b2a76226fa9da967218e888637970d2b
Duncan Robertson <duncan@whomwah.com>
2009-10-28 10:06:07 +0000

    let see of it still works with hpricot

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'rubygems' 
-require 'nokogiri'
+require 'hpricot'
 require 'open-uri'
 require 'sinatra'
 require 'erb'
@@ -38,17 +38,18 @@ class ShoppingList
     # need for these pages to be fixed. Here I'm just storing
     # a loose data structure 
 
-    @title = @doc.css('title').first.content.split(':').last
+    @title = (@doc/'title').inner_html.split(':').last.strip
 
     ignore_next_p = false 
-    @doc.css('div[class*="content-main"] > div.promo > *').each do |el|
-      break if el.node_name == 'div'
+    (@doc/'div.content-main > div.promo > *').each do |el|
+      break if el.name == 'div' || el.comment?
+      next if el.inner_text.strip == ''
 
-      if el.node_name == 'h2'
-        if el.content =~ /description|method/i
+      if el.name == 'h2'
+        if el.inner_text =~ /description|method/i
           ignore_next_p = true
           next
-        elsif el.content =~ /ingredients/i
+        elsif el.inner_text =~ /ingredients/i
           next
         end
       end
@@ -65,39 +66,24 @@ class ShoppingList
   end
 
   def fetch_recipe(url) 
-    @doc = Nokogiri::HTML(open(url))
+    @doc = Hpricot(open(url))
   end
 
-  def to_txt
-    results = @data 
-
-    results.map! do |el|
-      if el.node_name == 'h2'
-        "#{el.content.strip}<br/><br/>"
-      else
-        el
-      end
-    end
-
-    results = results.to_s.gsub(/<strong>|<\/strong>/, '<br/>')
-    results = results.split(/<br\/*>/).map! {|i| i.strip.gsub(/<\/?[^>]*>/,'')}
-    results.join("\n").strip
-  end
-
-  def for_iphone
+  def for_html
     d = @data 
     results = []
 
     d.map! do |el|
-      if el.node_name == 'h2'
-        "#{el.content.strip}<br/><br/>"
+      if el.name == 'h2'
+        "#{el.strip}<br/><br/>"
       else
         el
       end
     end
 
+
     d = d.to_s.gsub(/<strong>/, '<br/>')
-    d = d.split(/<br\/*>/).map! {|i| i.strip.gsub(/<\/?[^>]*>/,'')}
+    d = d.split(/<br\s*\/*>/).map! {|i| i.strip.gsub(/<\/?[^>]*>/,'')}
 
     has_blank = false 
     d.each do |item|
@@ -124,16 +110,15 @@ class ShoppingList
 
 end
 
-get('/shoppinglist') { 
+get('/list') { 
   #response["Cache-Control"] = "max-age=86400, public" 
+  content_type 'text/html', :charset => 'utf-8'
   begin
     @data = ShoppingList.new(params["r"])
     if iphone_request?
-      content_type 'text/html', :charset => 'utf-8'
       erb :iphone
     else
-      content_type 'text/plain', :charset => 'utf-8'
-      @data.to_txt
+      erb :html
     end 
   rescue OpenURI::HTTPError => e
     halt "Oo-la-la! there was something wrong with the URL you provided" 
@@ -142,16 +127,6 @@ get('/shoppinglist') {
 
 get('/') { 
   #response["Cache-Control"] = "max-age=86400, public" 
-  content_type 'text/plain', :charset => 'utf-8'
-  <<EOF
-Shopping List
--------------
-
-This app attempts to take a BBC Recipe url from http://XXX and convert it
-into a list of ingedients that can be viewed on your phone, specifically
-the iPhone because that's what I've got.
-
-http://#{request.env['HTTP_HOST']}/shoppinglist?r=
-
-EOF
+  content_type 'text/html', :charset => 'utf-8'
+  erb :index
 }
